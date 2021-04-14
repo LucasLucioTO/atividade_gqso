3afa8c212e67f3beccdd38703eab4685939c70e0
LucasLucioTO <LucasLucioT.deOliveira@gmail.com>
2021-04-14 16:21:55 -0300

    criação da classe Banco e método saque

--- ifal/src/main/java/atividade/Banco.java
+++ ifal/src/main/java/atividade/Banco.java
@@ -1,23 +1,9 @@
 package atividade;
 
 public class Banco {
-    private double saldo;
+    private double saldo = 0;
 
-    
+    public void saque(double saque) {
 
-    public class SaldoInsuficiente extends Exception{
-        private static final long serialVersionUID = 1L;
     }
-
-    public class ValorNegativo extends Exception{
-        private static final long serialVersionUID = 1L;
-    }
-
-    public void deposito (double deposito) throws ValorNegativo {
-        
-    } 
-
-    public void saque (double saque) {
-        return ;
-    }   
 }
